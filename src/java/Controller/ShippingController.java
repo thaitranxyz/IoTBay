c1d1856e32a8b89602a7ee93857429c4a75fd757
superfatboy <55424397+superfatboy@users.noreply.github.com>
2020-06-09 06:50:35 +1000

    Add files via upload

---
+++ src/java/Controller/ShippingController.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+     import java.sql.SQLException;
+     import java.util.logging.Level;
+     import java.util.logging.Logger;
+     import javax.servlet.ServletException;
+     import javax.servlet.http.HttpServlet;
+     import javax.servlet.http.HttpServletRequest;
+     import javax.servlet.http.HttpServletResponse;
+     import javax.servlet.http.HttpSession;
+     import DAO.Model.User;
+     import DAO.DBConnector;
+     import java.io.IOException;
+import java.sql.Connection;
+
+/**
+ *
+ * @author Jack
+ */
+public class ShippingController extends HttpServlet {
+    
+    private User user;
+    
+    @Override //Create and instance of DBConnector for the deployment session
+    public void init() {
+        try {
+            DBConnector db = new DBConnector();
+            Connection conn = db.openConnection();
+            user = new User(conn);
+
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(ConnServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+     @Override   
+     protected void doPost(HttpServletRequest request, HttpServletResponse response)   throws ServletException, IOException {       
+         
+         response.setContentType("text/html;charset=UTF-8");
+         HttpSession session = request.getSession();
+         String address = request.getParameter("address");
+         int userId = Integer.parseInt(request.getParameter("Id"));
+         
+             
+         
+             User user = user.findUser(userId);
+             
+             if(user != null) {
+                 user.setAddress(address);
+                 session.setAttribute("user", user);
+                 user.updateShipping(userId, address);
+                 System.out.println(user.getAddress());
+                 session.setAttribute("updated","Update was successful");
+                 request.getRequestDispatcher("shipping.jsp").include(request,response);
+             }
+             else {
+                     session.setAttribute("updated","Update was not successful");
+                     request.getRequestDispatcher("shipping.jsp").include(request,response);
+                     }
+             }
+        
+         
+         response.sendRedirect("shipment.jsp");
+}
+}
---
+++ src/java/Controller/ShippingServlet.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import DAO.Model.User;
+import DAO.DBConnector;
+/**
+ *
+ * @author Jack
+ */
+@WebServlet(name = "ShippingServlet", urlPatterns = {"/ShippingServlet"})
+public class ShippingServlet extends HttpServlet {
+    
+    private User user;
+    
+    @Override //Create and instance of DBConnector for the deployment session
+    public void init() {
+        try {
+            DBConnector db = new DBConnector();
+            Connection conn = db.openConnection();
+            user = new User(conn);
+
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(ConnServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)   throws ServletException, IOException {   
+        
+        HttpSession session = request.getSession();
+        int userId = Integer.parseInt(request.getParameter("id"));
+        user = (User) session.getAttribute("user");
+        
+        
+       
+            User user = user.findUser(userId);
+            if (user != null) {
+                session.setAttribute("user", user);
+                System.out.println(user.getAddress());
+                request.getRequestDispatcher("shipping.jsp").include(request,response);
+            } else {
+                session.setAttribute("existErr","User does not exist in the Database");
+                request.getRequestDispatcher("shipping.jsp").include(request,response);
+            }
+        
+        
+       
+        
+        
+    }
+
+    
+
+}
