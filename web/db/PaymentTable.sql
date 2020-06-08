f5b2dfa55c014cba8790e9dac9a6ec71924a2f71
pikachu-xyz <53890156+pikachu-xyz@users.noreply.github.com>
2020-06-08 12:11:15 +0800

    Add files via upload

---
+++ web/db/PaymentTable.sql
@@ -0,0 +1,18 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  pikachu125
+ * Created: 06/06/2020
+ */
+CREATE TABLE Payment
+(creditCardNumber VARCHAR(16) NOT NULL,
+customerId INT NOT NULL,
+creditCardExpiry VARCHAR(5),
+creditCardCVC VARCHAR(3),
+isDefault int,
+PRIMARY KEY (creditCardNumber),
+FOREIGN KEY (customerId) REFERENCES CUSTOMER(customerId)
+);
